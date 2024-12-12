750e17785877c9a32329ee4b365098fbff2dc081
“suryagarg” <suryashukla871@gmail.com>
2024-12-12 11:30:47 +0530

    Initial commit

---
+++ routes/course.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+// const router = express.Router
+
+const {Router} = require("express");
+
+const courseRouter = Router();
+
+courseRouter.get("/purchase", function(req, res){
+    
+})
+
+courseRouter.post("/purchasedCourse", function(req, res){
+    
+})
+
+
+courseRouter.post("/preview", function(req, res){
+    
+})
+
+module.exports = {
+    courseRouter: courseRouter
+}
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const router = express.Router;    
+
+const { Router } = require("express"); //other way of doing above
+
+const userRouter = Router();
+
+userRouter.post("/signup", function(req, res){
+
+})
+
+userRouter.post("/login", function(req, res){
+    
+})
+
+module.exports = {
+    userRouter: userRouter
+}
